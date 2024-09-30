d3f1f613f63502d6c22a2fa7496cd42b1264726c
calliee1 <CAddy2024@elam.co.uk>
2024-09-30 15:35:54 +0100

    destroyer fix :D

--- Assets/scripts/Destroyer.cs
+++ Assets/scripts/Destroyer.cs
@@ -6,7 +6,7 @@ public class Destroyer : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Cleanup") 
+        if (collision.gameObject.tag == "CleanUp") 
         {
             Destroy(collision.gameObject);
         }
